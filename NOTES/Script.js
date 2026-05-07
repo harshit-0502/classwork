e5bdb03871a6d73b5a685ea58db7c545ff4ea61e
Gopal Maheshwari <gopalbirla181@gmail.com>
2026-05-07 12:12:39 +0530

    Notes

---
+++ NOTES/Script.js
@@ -0,0 +1,51 @@
+// console.log("hello world");
+// // Introduction to Variables:  1. Var ;
+// {
+//   var a = 20;
+//   a = "Gopal";
+//   console.log(a);
+// }
+
+/*(function (x) {
+    console.log(x);
+})("Hi, Gopal");
+*/
+
+//2.const
+/*{
+const name= "Gopal";
+console.log(name, " blah blah");
+}
+    console.log(name);
+*/
+// 3. Let
+
+
+/* arithmetic operator 
+let a = 10;
+let b = 5;
+console.log(a+b); // 15
+console.log(a-b); // 5
+console.log(a*b); // 50
+console.log(a/b); // 2
+console.log(a**b); // 100000
+console.log(a++); //10 
+console.log(++a); //12
+console.log(--a); //11
+console.log(a--); //11
+console.log(a);*/
+
+// Assigment operator 
+
+/*let a=10;
+a+=2;
+console.log(a);
+a-=2;
+console.log(a);
+a*=2;
+console.log(a);
+a/=2; 
+console.log(a);
+*/
+
+
